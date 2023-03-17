4b4f3e5c6a0f6168879d0422554aa54ef98234e7
1YahAA <yahaa040404@yandex.ru>
2023-03-17 22:54:21 +1000

    добавила переменную интовую а

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,9 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+            int a = 4;
+            Console.WriteLine("a");
+             
         }
     }
 }
