a1d09a608fa6a4ef0c0da39bd5ce764eaf5edf9d
1YahAA <yahaa040404@yandex.ru>
2023-03-27 19:46:50 +1000

    GameMenu

---
+++ MainMENU/Program.cs
@@ -0,0 +1,21 @@
+﻿//WriteLine($"{NameHero} у тебя стихия Огня\nИ вот столько хп {Fire.HP}");
+
+//         class Fire {
+//             public static int HP { get; set;}
+//         }
+
+//     }
+// }
+
+
+
+using System;
+
+namespace Game {
+    class Program {
+        static void Main(string[] args) {
+            Game myGame = new Game();
+            myGame.Start();
+        }
+    }
+}
