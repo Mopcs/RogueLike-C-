efc3697b6daedf43cb90d92a1f5efea3e5a92bbb
ove4lo <alina.mik.kz@gmail.com>
2023-04-02 14:10:14 +1000

    Update alinaprogramm.cs
    change if

--- alinaprogramm.cs
+++ alinaprogramm.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 
@@ -32,21 +33,13 @@ namespace zodiak_labirint
                 {
                     for (int j = 1; j < 19; j++)
                     {
-                        if (i == 0)
+                        if (i == 0 || j == 0 || i == 19 || j == 19)
                         {
                             MapArray[i, j] = '#';
                         }
-                        else if (j == 0)
+                        else if (i == 1 && j == 2)
                         {
-                            MapArray[i, j] = '#';
-                        }
-                        else if (i == 19)
-                        {
-                            MapArray[i, j] = '#';
-                        }
-                        else if (j == 19)
-                        {
-                            MapArray[i, j] = '#';
+                            MapArray[i, j] = 'X';
                         }
                         else
                         {
@@ -362,6 +355,34 @@ namespace zodiak_labirint
         }
     }
 
+        public class Zodiak
+    {
+        //игра
+        public static void Game()
+        {
+            Console.SetWindowSize(Maze.GameWidth, Maze.GameHeight); //размер окна по заданным параматрам
+            Console.SetBufferSize(Maze.GameWidth, Maze.GameHeight); //размер буфера по заданным параметрам
+
+            Console.CursorVisible = false; //выключение курсора
+
+            Maze.Init(); //иницилизация карты
+
+            while (true)
+            {
+                Player.Steps(); //шаги игрока
+
+                Render3D.Draw(); //отрисовка лабиринта и его составляющих
+
+                Maze.Draw2D(); //отрисовка карты
+                Maze.Stats(); //состояние
+
+                Console.SetCursorPosition(0, 0);
+                Console.Write(Maze.Screen);
+
+            }
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
